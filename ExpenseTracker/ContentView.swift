ae08fdfe1ab0f9d0fe665ab130aa85ed7a8f8ca7
HorevyiNazar <goreviynazar@gmail.com>
2024-01-21 12:22:33 +0100

    Part 1

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Intro Visibility Status
+    @AppStorage("isFirstTime") private var isFirstTime: Bool = true
+    // Active Tab
+    @State private var activeTab: Tab = .recents
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $activeTab) {
+            Text("Recents")
+                .tag(Tab.recents)
+                .tabItem { Tab.recents.tabContent }
+            
+            Text("Search")
+                .tag(Tab.search)
+                .tabItem { Tab.search.tabContent }
+            
+            Text("Chart")
+                .tag(Tab.charts)
+                .tabItem { Tab.charts.tabContent }
+            
+            Text("Settings")
+                .tag(Tab.settings)
+                .tabItem { Tab.settings.tabContent }
         }
-        .padding()
+        .tint(appTint)
+        .sheet(isPresented: $isFirstTime, content: {
+            IntroScreen()
+                .interactiveDismissDisabled()
+        })
     }
 }
 
---
+++ ExpenseTracker/Helpers/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  ExpenseTracker
+//
+//  Created by Назар Горевой on 21/01/2024.
+//
+
+import SwiftUI
+
+let appTint: Color = .blue
